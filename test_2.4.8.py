2362ee76be2ab40b575f4ab337547f5790b67a22
vpomelov <vpomelov@htc-cs.ru>
2020-06-16 12:47:55 +0400

    Внесение_комментариев_в_последнее_задание_предыдущего_блока

--- test_2.4.8.py
+++ test_2.4.8.py
@@ -12,19 +12,19 @@ browser.get(link)
 
 try:
 
-    wait = WebDriverWait(browser, 14)
-    wait.until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
-    button = browser.find_element_by_id("book")
-    button.click()
+    wait = WebDriverWait(browser, 14) #объявление ожидания в 14 секунд
+    wait.until(EC.text_to_be_present_in_element((By.ID, "price"), "$100")) #будем ждать пока сумма не станет 100 долларов
+    button = browser.find_element_by_id("book") #находим кнопку
+    button.click() #жмем на кнопку
 
     def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
+        return str(math.log(abs(12 * math.sin(int(x))))) #формула для вычисления
 
-    data = browser.find_element_by_id("input_value")
-    x = data.text
-    Y = calc(x)
+    data = browser.find_element_by_id("input_value") #объявление поля с данными X
+    x = data.text #вытягивем данные из Х в виде текста
+    Y = calc(x) #вычисление
 
-    answer = browser.find_element_by_id("answer")
+    answer = browser.find_element_by_id("answer") 
     answer.send_keys(Y)
 
     button1 = browser.find_element_by_id("solve")
